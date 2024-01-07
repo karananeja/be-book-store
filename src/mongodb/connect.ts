21f8b474f83a63dbf15a21aa08a2a62e19079b19
karananeja <karananeja.r@gmail.com>
2024-01-07 21:07:39 +0530

    removed the logs

--- src/mongodb/connect.ts
+++ src/mongodb/connect.ts
@@ -2,7 +2,6 @@ import { connect } from 'mongoose';
 
 export const connectDB = async (url: string) => {
   try {
-    console.log({ url });
     await connect(url);
     console.log('[database]: Connection is set up with MongoDB');
   } catch (error) {
