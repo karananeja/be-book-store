cde7e0eb3d1254c5e3505038899ce09d74d7bade
karananeja <karananeja.r@gmail.com>
2024-01-07 17:43:53 +0530

    created the required types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,29 @@
+import { Response } from 'express';
+
 export type EnvironmentTypes = {
   DB_PASSWORD?: string;
   DB_USERNAME?: string;
   DB_NAME?: string;
   APP_PORT?: string;
+};
+
+export type BookType = {
+  title: string;
+  author: string;
+  publishYear: number;
+};
+
+type ErrType = { err: string; errMessage: string };
+
+export type ResponseStructureType = {
+  data: ErrType;
+  res: Response;
+  statusCode: number;
+};
+
+export type ErrMessagesType = {
+  [x: string]: {
+    err: string;
+    errMessage: string;
+  };
 };
