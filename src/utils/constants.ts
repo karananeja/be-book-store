9d92740be462c9fb483a1864ac0962cd1e0e9ad5
karananeja <karananeja.r@gmail.com>
2024-01-07 16:03:27 +0530

    created constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+import { EnvironmentTypes } from './types';
+
+const environment: EnvironmentTypes = {
+  DB_PASSWORD: process.env.NODE_DB_PASSWORD,
+  DB_NAME: process.env.NODE_DB_NAME,
+  APP_PORT: process.env.NODE_APP_PORT,
+};
+
+export { environment };
